cf60b5f341ed3b7893f6af0beb8a4eb8252699d6
Rahul Shetty <rashetty@redhat.com>
2025-04-15 19:07:15 +0530

    remove hardcoding of model template and switch to openai client library

--- src/llm/llm_client.py
+++ src/llm/llm_client.py
@@ -1,28 +1,54 @@
 import os
-import httpx
 
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from openai import OpenAI
+
+from src.modals.llm_data import LLM_Client_Type
 
 
-def get_openai_client() -> ChatOpenAI:
+def query_openai_client(model: str, client: OpenAI, system_prompt: str, user_prompt: str) -> str:
     '''
     Returns OpenAI compliant client
     '''
-    load_dotenv()
-
-    OPENAI_API_BASE = os.getenv('OPENAI_API_BASE')
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    MODEL = os.getenv('MODEL')
-
-    http_client = httpx.Client()
-
-    return ChatOpenAI(
-        openai_api_base=OPENAI_API_BASE,
-        openai_api_key=OPENAI_API_KEY,
+    completion = client.chat.completions.create(
+        model=model,
+        messages=[
+            {"role": "assistant", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
         temperature=0,
         top_p=1,
-        http_client=http_client,
         max_tokens=512,
-        model=MODEL
     )
+    return completion.choices[0].message.content
+
+
+class LLM_Client:
+    '''
+    Generic class to handle calls to LLM services.
+    '''
+    def __init__(self, llm_type: LLM_Client_Type = LLM_Client_Type.openai):
+        self.type = llm_type
+        if llm_type == LLM_Client_Type.openai:
+            self.__init_openai__()
+
+    def __init_openai__(self):
+        load_dotenv()
+        OPENAI_API_BASE = os.getenv('OPENAI_API_BASE')
+        OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+        self.model = os.getenv('MODEL')
+        self.openai_client = OpenAI(
+            base_url=OPENAI_API_BASE,
+            api_key=OPENAI_API_KEY
+        )
+
+    def invoke(self, sys_prompt: str, user_prompt: str):
+        '''Returns the result from LLM Service'''
+        if self.type == LLM_Client_Type.openai:
+            return query_openai_client(
+                model=self.model,
+                client=self.openai_client,
+                system_prompt=sys_prompt,
+                user_prompt=user_prompt
+            )
+        raise Exception("Unknown model type!")
